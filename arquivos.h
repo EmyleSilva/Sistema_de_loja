0189f114747024524c7dfaafa3f2b417e75c2011
EmyleSilva <emylesnt.silva@gmail.com>
2023-11-22 20:24:21 -0300

    documentação do cabeçalho de arquivos

--- arquivos.h
+++ arquivos.h
@@ -2,10 +2,36 @@
 #define ARQUIVOS_H_INCLUDED
 
 #include "produtos.h"
+#include "clientes.h"
 
+/*
+** Funções que inicializam os produtos, clientes e vendas;
+** Quando o sistema é aberto, a função inicia o mesmo
+** com valores recuperados dos respectivos arquivos.
+*/
 Produtos* inicializaProdutos(char *nomeArq, int *quant);
+Clientes* inicializaClientes(char *nomeArq, int *quant);
+
+/*
+** Função que passa os valores dos arquivos para as
+** listas encadeadas do sistema
+*/
 Produtos *iniciaListaProd(Produtos *ini_prod, Produtos *aux, int quant);
+Clientes *iniciaListaClientes(Clientes *ini_c, Clientes *aux, int quant);
+
+/*
+** Lê os valores armazenados nos arquivos, e salva em um vetor
+** que é retornado para que os demais procedimentos possam ser
+** realizados
+*/
 Produtos* lerArquivosProdutos(char *nomeArq, int quantidade_p);
-FILE* salvarProdutos(char *nomeArq, int quant_p, Produtos *ini_p);
+Clientes* lerArquivosClientes(char *nomeArq, int quantidade_c);
+
+/*
+** Procedimentos que salvam os valores do sistema em arquivos
+** durante a execução
+*/
+void salvarProdutos(char *nomeArq, int quant_p, Produtos *ini_p);
+void salvarClientes(char *nomeArq, int *quant_c, Clientes *ini_c);
 
 #endif // ARQUIVOS_H_INCLUDED
